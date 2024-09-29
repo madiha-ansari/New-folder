b4ceb32edad0045c64803b03a367d5dc93d870a9
madiha-ansari <157696198+madiha-ansari@users.noreply.github.com>
2024-09-29 08:58:59 +0500

    pracctice

---
+++ app.js
@@ -0,0 +1,35 @@
+// ch 52 
+// Manipulating images and text
+
+// function update_dbtn(){
+// var main = document.getElementById("main");
+// main.className += " bgGreen";
+// }
+
+// ---------------------------------------------------------end--------------------------------
+
+// cls 20 55 ch sir ghous
+
+// 53
+// Swapping images
+// function swapPic() {
+//     document.getElementById("before").src = "gh2.jpg";
+// }
+
+
+// 54
+// Swapping images and setting classes
+// function swapPic()  {
+// document.getElementById("before").src = "gh2.jpg";
+// }
+
+// function getAddress() {
+//     var link = document.getElementById("link1");  // Yeh link ko dhoondta hai
+//     var address = link.href;  // Yeh us link ka href address nikalta hai
+//    document.write(address);  // Console mein address dikhata hai
+// }
+
+// function enlargeForm() {
+//     var frm = document.getElementById("form");  // Yeh form ko dhoondta hai
+//     frm.className += " jumbo";  // Form ka class badal ke jumbo kar deta hai
+//   }
